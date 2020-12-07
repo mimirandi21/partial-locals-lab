62f141d8e6f12e8e6b0cb8dc1d2165b00b9a9e1e
Randi Schreiner <mimirandi21@gmail.com>
2020-12-07 10:50:28 -0600

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,12 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+  def self.search(input)
+    if input == ''
+      @students = Student.all
+    else 
+      @students = Student.where("name like ?", "%#{input}%")
+    end
+  end
 end
